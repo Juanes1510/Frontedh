ac07a035dd0f75d56d9b46156e2ea79208539993
jhonathanstiven <jhonathanbolanos10@gmail.com>
2024-10-12 11:11:56 -0500

    almacenamiento de tokens en cokies

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,12 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
+
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
   const isToken = request.cookies.get("token");
+  
+  
   console.log({ isToken });
 
   if (!isToken) {
